ec792a314d4446d0843cb5115d40f6d42bf6b706
claude776 <codeclaude2@gmail.com>
2025-06-14 22:08:08 +0200

    Future is Loading 1337

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -10,25 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nb)
-
+int	ft_recursive_factorial(int nb)
 {
 	int	result;
 
 	result = 1;
 	if (nb < 0)
 		return (0);
-	while (nb > 0)
+	if (nb > 0)
 	{
-		result *= nb;
-		nb--;
+		
 	}
-	return (result);
 }
-/*#include <stdio.h>
-
-int	main(int argc, char argv[])
-{
-	printf("%d", ft_iterative_factorial(5));
-	printf("%d", ft_iterative_factorial(0));
-}*/
