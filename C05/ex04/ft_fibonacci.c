d7d2ab7790373d25773df8b145580299866a33dc
claude776 <codeclaude2@gmail.com>
2025-07-02 00:15:25 +0200

    Futur is Loading

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -11,13 +11,19 @@
 /* ************************************************************************** */
 
 int	ft_fibonacci(int index)
-
 {
 	if (index < 0)
 		return (-1);
-	if (index > 0)
-	{
-		ind
-ex = index + 1 + 2;
-	}
+	else if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
+/*#include<stdio.h>
+
+int	main(int argc, char argv[])
+{
+	int index = 5;
+	printf("%d le fibonacci du nombre", ft_fibonacci(index));
+}*/
